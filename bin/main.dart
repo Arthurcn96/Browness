1894b5e9069afbb778a2697c6aef4eec6c758f41
Arthurcn96 <arthurcn96@gmail.com>
2021-12-01 21:53:16 -0300

    Implementando binarizaçao de imagem

--- bin/main.dart
+++ bin/main.dart
@@ -4,15 +4,24 @@ import 'package:image/image.dart';
 import 'Filtro.dart';
 
 void main() {
-  List<int> aux;
+  // List<int> aux;
   var filtro = Filtro();
 
-  // Roda em i fotos do assets o browness com k = 0 até j
-  for (var i = 0; i < 1; i++) {
-    for (var j = 0; j < 5; j++) {
-      final image = decodePng(File('assets/IMD$i.png').readAsBytesSync())!;
-      aux = filtro.intensidadeMaxMin(image, j);
-      filtro.browness(image, j, FiltroName.Teorico, i, aux[0], aux[1]);
-    }
+  // // Roda em i fotos do assets o browness com k = 0 até j
+  // for (var i = 0; i < 1; i++) {
+  //   for (var j = 0; j < 5; j++) {
+  //     final image = decodePng(File('assets/IMD$i.png').readAsBytesSync())!;
+  //     aux = filtro.intensidadeMaxMin(image, j);
+  //     filtro.browness(image, j, FiltroName.Teorico, i, aux[0], aux[1]);
+  //   }
+  // }
+
+  final image = decodePng(File('assets/M.png').readAsBytesSync())!;
+
+  try {
+    filtro.binarizacao(image);
+  } catch (exception) {
+    print('Erro: ');
+    print(exception);
   }
 }
